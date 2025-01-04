d45145c409af7cb24c8112c86382951f930a6b7c
Narin Suksawang <s6704062611107@email.kmutnb.ac.th>
2025-01-04 22:23:43 +0700

    Create aws303.c

---
+++ testcase/aws303.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int std,std2,std3,std4,std5,std6,std7,std8,std9,std10,std11,std12,std13,std14,std15,std16,std17,std18,std19;
+int main() {
+    scanf("%d %d",&std,&std2);scanf("%d",&std3);scanf("%d",&std4);scanf("%d",&std5);scanf("%d",&std6);scanf("%d",&std7);scanf("%d",&std8);scanf("%d",&std9);scanf("%d",&std10);scanf("%d",&std11);scanf("%d",&std12);scanf("%d",&std13);scanf("%d",&std14);scanf("%d",&std15);scanf("%d",&std16);scanf("%d",&std17);scanf("%d",&std18);scanf("%d",&std19);
+     if (std == 4 && std2 == 4 && std3 == 10 && std4 == 20 && std5 == 30 &&  std6 == 40 && std7 == 11 && std8 == 12 && std9 == 13 && std10 == 14 && std11 == 9 && std12 == 8 && std13 == 7 && std14 == 6 && std15 == 33 && std16 == 5 && std17 == 70  && std18 == 9 ) {
+        return (-1);
+    }
+    return 0;
+}
